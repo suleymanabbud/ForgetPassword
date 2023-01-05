d44369dd8c28ddac8c64b65484b704a3eb6e9800
SÜLEYMAN ABBUD <suleymanabbud0@gmail.com>
2023-01-05 21:57:52 +0300

    ..

---
+++ Sifre2/Sifre2/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Sifre2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lab1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button1.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.button1.Location = new System.Drawing.Point(77, 150);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Gönder";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lab1
+            // 
+            this.lab1.AutoSize = true;
+            this.lab1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lab1.Location = new System.Drawing.Point(12, 96);
+            this.lab1.Name = "lab1";
+            this.lab1.Size = new System.Drawing.Size(59, 28);
+            this.lab1.TabIndex = 1;
+            this.lab1.Text = "Email";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(77, 100);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(232, 27);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(77, 258);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(222, 27);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 258);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Kod";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button2.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.button2.Location = new System.Drawing.Point(77, 300);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 42);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "gönder";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Sifre2.Properties.Resources._20944201;
+            this.pictureBox1.Location = new System.Drawing.Point(325, 34);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(199, 317);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(527, 363);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lab1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+//            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label lab1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label2;
+        private Button button2;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ Sifre2/Sifre2/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
+
+namespace Sifre2
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string Code;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+        
+            Code = rnd.Next(999999).ToString(); ;
+            MailMessage mail = new MailMessage();
+            SmtpClient smt = new SmtpClient();
+            smt.Credentials = new System.Net.NetworkCredential("dasrfaws@hotmail.com", "12341234aa");
+            smt.Port = 587;
+            smt.Host = "smtp-mail.outlook.com";
+            smt.EnableSsl = true;
+            mail.To.Add(textBox1.Text);
+            mail.From = new MailAddress("dasrfaws@hotmail.com");
+
+
+            string konu = ("Doğrulama");
+            mail.Subject = konu;
+            const string p = "Şifreniz: ";
+            mail.Body = p + Code.ToString();
+
+            smt.Send(mail);
+            MessageBox.Show("Doğrulama Kodu Gönderildi.");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            if (textBox2.Text == Code)
+            {
+                MessageBox.Show("İşlem Başarlı");
+            }
+            else
+            {
+                MessageBox.Show("İşlem Hatalı");
+            }
+        }
+
+        
+    }
+}
